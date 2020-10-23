7e1b02bcfd8cda6ab5b06d9f91cd01f53f338df2
Michelle Moon <robbins.michelle@ymail.com>
2020-10-22 19:31:34 -0500

    heroku trial

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 
@@ -65,7 +65,7 @@ app.post("/api/notes", function (req, res) {
 });
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 
